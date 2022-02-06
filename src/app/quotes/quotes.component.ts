5d2396d58d219c2d08d85417c011b6018b933fed
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-02-06 16:01:10 +0300

    Updated quote form'

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,11 +9,11 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes [] = [
-    new Quotes (1,'Caleb Langat', 'Riesling', 'Always very high in acid, when made as a table wine Rieslings can be harmoniously sweet (sweet and sour) or dry (very acidic). The wine is polarizing because some people find dry styles too acidic and sweet styles too cloying, but sweetness is always a wine making decision and not inherent to the grape.'),
-    new Quotes (2,'Caleb Langat', 'Syrah', ' Syrah (aka Shiraz) is a full-bodied red wine thats heavily planted in the Rhône Valley in France and Australia. The wines have intense fruit flavors and medium-weight tannins. Syrah is commonly blended with Grenache and Mourvèdre to create the red Rhône blend. The wine often has a meaty (beef broth, jerky) quality.'),
-    new Quotes (3,'Caleb Langat', 'Zinfandel', ' Zinfandel (aka Primitivo) is a medium-bodied red wine that originated in Croatia. Wines are fruit-forward and spicy with a medium length finish. Zinfandel is a red grape that may be better known in its pink variation, White Zinfandel.'),
-    new Quotes (4,'Caleb Langat', 'Chardonnay', 'Chardonnay is a dry full-bodied white wine that was planted in large quantities for the first time in France. When oak-aged, Chardonnay will have spicy, bourbon-y notes. Unoaked wines are lighter and zesty with apple and citrus flavors. Chardonnay is the white grape of Burgundy.')
-  ]
+    new Quotes (1,'Caleb Langat', 'RIESLING', 'Always very high in acid, when made as a table wine Rieslings can be harmoniously sweet (sweet and sour) or dry (very acidic). The wine is polarizing because some people find dry styles too acidic and sweet styles too cloying, but sweetness is always a wine making decision and not inherent to the grape.'),
+    new Quotes (2,'Caleb Langat', 'SYRAH', ' Syrah (aka Shiraz) is a full-bodied red wine thats heavily planted in the Rhône Valley in France and Australia. The wines have intense fruit flavors and medium-weight tannins. Syrah is commonly blended with Grenache and Mourvèdre to create the red Rhône blend. The wine often has a meaty (beef broth, jerky) quality.'),
+    new Quotes (3,'Caleb Langat', 'ZINFANDEL', ' Zinfandel (aka Primitivo) is a medium-bodied red wine that originated in Croatia. Wines are fruit-forward and spicy with a medium length finish. Zinfandel is a red grape that may be better known in its pink variation, White Zinfandel.'),
+    new Quotes (4,'Caleb Langat', 'CHARDONNAY', 'Chardonnay is a dry full-bodied white wine that was planted in large quantities for the first time in France. When oak-aged, Chardonnay will have spicy, bourbon-y notes. Unoaked wines are lighter and zesty with apple and citrus flavors. Chardonnay is the white grape of Burgundy.')
+  ];
 
   toggleDetails(index:number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
